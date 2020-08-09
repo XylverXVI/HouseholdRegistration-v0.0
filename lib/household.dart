23c547e4a4ee4cbdb915021507b737feb96c345e
Fortunato Sam Balindan <fortunebalindan@gmail.com>
2020-08-09 14:42:02 +0800

    App Files

---
+++ lib/household.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HHRegistrationForm extends StatefulWidget {
+  @override
+  _HHRegistrationFormState createState() => _HHRegistrationFormState();
+}
+
+class _HHRegistrationFormState extends State<HHRegistrationForm> {
+  final _formKey = GlobalKey<FormState>();
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: <Widget>[
+          
+        ],
+      ),
+    );
+  }
+}
