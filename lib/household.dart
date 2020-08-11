9b925082ca12fd28a70a81bee2ae69a0b22229a2
Fortunato Sam Balindan <fortunebalindan@gmail.com>
2020-08-11 19:01:40 +0800

    App Files

--- lib/household.dart
+++ lib/household.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter/cupertino.dart';
 
 class HHRegistrationForm extends StatefulWidget {
   @override
@@ -9,13 +11,110 @@ class _HHRegistrationFormState extends State<HHRegistrationForm> {
   final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
-    return Form(
-      key: _formKey,
-      child: Column(
-        children: <Widget>[
-          
-        ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Household Registration"),
+      ),
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Text(
+                    "Household Serial Number",
+                    style: styleText,
+                  ),
+                  Expanded(
+                    
+                    child: SizedBox(
+                      width: 200,
+                      height: 50,
+                      child: TextField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                children: <Widget>[
+                  Text(
+                    "Region",
+                    style: styleText,
+                  ),
+                  Expanded(
+                    child: SizedBox(
+                      width: 200,
+                      child: TextField(),
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                children: <Widget>[
+                  Text(
+                    "Province",
+                    style: styleText,
+                  ),
+                  Expanded(
+                    child: SizedBox(
+                      width: 200,
+                      child: TextField(),
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                children: <Widget>[
+                  Text(
+                    "City/Municipality",
+                    style: styleText,
+                  ),
+                  Expanded(
+                    child: SizedBox(
+                      width: 200,
+                      child: TextField(),
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                children: <Widget>[
+                  Text(
+                    "Barangay",
+                    style: styleText,
+                  ),
+                  Expanded(
+                    child: SizedBox(
+                      width: 200,
+                      child: TextField(),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
+}
+
+TextStyle styleText = GoogleFonts.openSans(
+  color: Colors.black,
+  fontWeight: FontWeight.bold,
+  fontSize: 15,
+);
+
+filler(double h, double w) {
+  return SizedBox(
+    height: h,
+    width: w,
+  );
 }
